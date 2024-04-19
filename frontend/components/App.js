8bddcf342099adc61e81360717e425e5db1b2da4
nabeelmumtaz92 <nabeelmumtaz92@gmail.com>
2024-04-19 18:34:25 -0500

    commit

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -7,7 +7,7 @@ import Navbar from "./Navbar"
 
 const App = () => {
   const [coinData, setCoinData] = useState([])
-  const [darkMode, setDarkMode] = useState(false)
+  const [darkMode, setDarkMode] = useDarkMode(false)
 
   useEffect(() => {
     axios
---
+++ frontend/hooks/useDarkMode.js
@@ -0,0 +1,6 @@
+import {useState} from 'react'
+import {useLocalStorage} from './useLocalStorage'
+export function useDarkMode () {
+    const [dark, setDark] = useLocalStorage('darkmode', false)
+    return [dark, setDark]
+}
